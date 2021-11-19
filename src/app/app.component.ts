745ed6713b28527775bea62b5edb2a487bf1ea58
kabweobeed <kabweobed@gmail.com>
2021-11-18 21:57:04 -0300

    usar event-binding e interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'projectest';
+  nome = 'obed kabwe';
+
+
+  adicionar(){
+    console.log('Adicionar:'  + this.nome );
+  
+    const number = Math.trunc(Math.random()*100) ;
+    
+    this.nome= "oberson" + number;
+  }
+
+ 
+ 
+
+
+
+
+
+
+
+
+
+
+
 }
